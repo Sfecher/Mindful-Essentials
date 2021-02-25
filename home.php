ab9607272e6a831e03c7baa5fffa10b28af5a05d
Shelbie Fecher <63127198+Sfecher@users.noreply.github.com>
2021-02-25 08:56:37 -0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>SKIN CARE</title>
+        <script src="myScript.js"></script>
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <img src="imgs/logo2.png" alt="logo" id="logo">   
+            <nav>
+                <a href="quiz.php" class="navLink">QUIZ</a> 
+                <a href="#" class="navLink">PRODUCTS</a> 
+                <a href="#" class="navLink">ABOUT US</a> 
+            </nav>
+        </div>
+        <img src="imgs/image1.png" alt="promotional image" id="personalize"> 
+        <div>
+            <button onclick="clickFunction()" type="button" id="button">Take Your Personalized Skin Care Quiz</button>
+        </div>
+
+    </body>
+</html>
