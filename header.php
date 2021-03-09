f4bb57693e8029f7f2b6d0480955bf22790c3a9a
Shelbie Fecher <63127198+Sfecher@users.noreply.github.com>
2021-03-09 13:03:00 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,10 @@
+<!-- This is the header of all of the pages -->
+<?php
+    echo <<<EOT
+    <head>
+        <title>SKIN CARE</title>
+        <script src="myScript.js"></script>
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+    EOT;
+?>
---
+++ nav.php
@@ -0,0 +1,15 @@
+<!-- This is the navigation for all of the pages -->
+<?php
+    echo <<<EOT
+    <div id="wrapper">
+        <a href="home.php"><img src="imgs/logo2.png" alt="logo" id="logo"></a> 
+        <nav>
+            <a href="quiz.php" class="navLink">QUIZ</a> 
+            <a href="products.php" class="navLink">PRODUCTS</a> 
+            <a href="aboutUs.php" class="navLink">ABOUT US</a> 
+        </nav>
+    </div>
+
+    EOT;
+?>
+
