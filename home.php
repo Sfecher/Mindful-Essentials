21b2e7bd51278cc5a9488c13fa0af9ee49509a39
Shelbie Fecher <63127198+Sfecher@users.noreply.github.com>
2021-03-09 15:23:25 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,19 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>SKIN CARE</title>
-        <script src="myScript.js"></script>
-        <link rel="stylesheet" href="stylesheet.css">
+        <link rel="icon" href="brain.jpg" type="image/x-icon">
     </head>
     <body>
-        <div id="wrapper">
-            <img src="imgs/logo2.png" alt="logo" id="logo">   
-            <nav>
-                <a href="quiz.php" class="navLink">QUIZ</a> 
-                <a href="#" class="navLink">PRODUCTS</a> 
-                <a href="#" class="navLink">ABOUT US</a> 
-            </nav>
-        </div>
+        <?php include ("header.php")?>
+        <?php include ("nav.php")?>
         <img src="imgs/image1.png" alt="promotional image" id="personalize"> 
         <div>
             <button onclick="clickFunction()" type="button" id="button">Take Your Personalized Skin Care Quiz</button>
